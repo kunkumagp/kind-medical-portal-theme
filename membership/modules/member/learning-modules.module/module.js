01bbe6446bc8825d752e513028c9c1d607d42aea
Kunkuma Geeth <kunkumagp@gmail.com>
2024-01-19 11:06:15 +0530

    2024-01-19-1

--- membership/modules/member/learning-modules.module/module.js
+++ membership/modules/member/learning-modules.module/module.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
-  $(".learning-modules").on("keydown", function(event) {
-      if(event.which == 13) {
-        $(".learning-module-form").submit();
-      }
-    });
+$(document).ready(function () {
+  $(".learning-modules").on("keydown", function (event) {
+    if (event.which == 13) {
+      var inputValue = $("input[name='query']").val();
+      console.log(inputValue);
+//       $(".learning-module-form").submit();
+    }
+  });
 });
--- membership/modules/member/our-products-filtering-system.module/module.js
+++ membership/modules/member/our-products-filtering-system.module/module.js
@@ -2,16 +2,84 @@
 $('#format').click(function() {
   $('#time-of-use-dropbox-container').hide();
   $('#time-of-use-dropbox-container-tag').hide();
-  
-  
+  $('#potency-dropbox-container').hide();
+  $('#potency-dropbox-container-tag').hide();
+  $('#Reported-effects-dropbox-container').hide();
+  $('#Reported-effects-dropbox-container-tag').hide();
+  $('#TGA-Category-dropbox-container').hide();
+  $('#TGA-Category-dropbox-container-tag').hide();
+
+
   $('#format-dropbox-container').toggle();
   $('#format-dropbox-container-tag').toggle();
 });
 
 $('#time-of-use').click(function() {
-    $('#format-dropbox-container').hide();
-    $('#format-dropbox-container-tag').hide();
-  
-    $('#time-of-use-dropbox-container').toggle();
-    $('#time-of-use-dropbox-container-tag').toggle();
+  $('#format-dropbox-container').hide();
+  $('#format-dropbox-container-tag').hide();
+  $('#potency-dropbox-container').hide();
+  $('#potency-dropbox-container-tag').hide();
+  $('#Reported-effects-dropbox-container').hide();
+  $('#Reported-effects-dropbox-container-tag').hide();
+  $('#TGA-Category-dropbox-container').hide();
+  $('#TGA-Category-dropbox-container-tag').hide();
+
+  $('#time-of-use-dropbox-container').toggle();
+  $('#time-of-use-dropbox-container-tag').toggle();
 });
+
+$('#potency').click(function() {
+  $('#format-dropbox-container').hide();
+  $('#format-dropbox-container-tag').hide();
+  $('#time-of-use-dropbox-container').hide();
+  $('#time-of-use-dropbox-container-tag').hide();
+  $('#Reported-effects-dropbox-container').hide();
+  $('#Reported-effects-dropbox-container-tag').hide();
+  $('#TGA-Category-dropbox-container').hide();
+  $('#TGA-Category-dropbox-container-tag').hide();
+
+  $('#potency-dropbox-container').toggle();
+  $('#potency-dropbox-container-tag').toggle();
+});
+
+$('#Reported-effects').click(function() {
+  $('#format-dropbox-container').hide();
+  $('#format-dropbox-container-tag').hide();
+  $('#time-of-use-dropbox-container').hide();
+  $('#time-of-use-dropbox-container-tag').hide();
+  $('#potency-dropbox-container').hide();
+  $('#potency-dropbox-container-tag').hide();
+  $('#TGA-Category-dropbox-container').hide();
+  $('#TGA-Category-dropbox-container-tag').hide();
+
+  $('#Reported-effects-dropbox-container').toggle();
+  $('#Reported-effects-dropbox-container-tag').toggle();
+
+});
+
+$('#TGA-Category').click(function() {
+  $('#format-dropbox-container').hide();
+  $('#format-dropbox-container-tag').hide();
+  $('#time-of-use-dropbox-container').hide();
+  $('#time-of-use-dropbox-container-tag').hide();
+  $('#potency-dropbox-container').hide();
+  $('#potency-dropbox-container-tag').hide();
+  $('#Reported-effects-dropbox-container').hide();
+  $('#Reported-effects-dropbox-container-tag').hide();
+  
+  $('#TGA-Category-dropbox-container').toggle();
+  $('#TGA-Category-dropbox-container-tag').toggle();
+
+});
+
+function getcheckedCheckboxCount(){
+   var numberNotChecked = $('#format input:checkbox:checked').length;
+   console.log(numberNotChecked);
+}
+
+getcheckedCheckboxCount();
+
+$("#format input:checkbox").change(function() {
+    getcheckedCheckboxCount();
+});
+
