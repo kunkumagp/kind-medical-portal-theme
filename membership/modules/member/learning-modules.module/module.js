1eb594c28238fbb7f1a5931102b847ec3543a64c
Kunkuma Geeth <kunkumagp@gmail.com>
2024-01-23 10:00:10 +0530

    .

--- membership/modules/member/learning-modules.module/module.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(".learning-modules").on("keydown", function (event) {
-    if (event.which == 13) {
-      var inputValue = $("input[name='query']").val();
-      console.log(inputValue);
-//       $(".learning-module-form").submit();
-    }
-  });
-});
