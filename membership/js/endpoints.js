5a26d3871961facb0b1178c2a48d2f96d847d61e
Kunkuma Geeth <kunkumagp@gmail.com>
2024-01-23 09:57:37 +0530

    2024-01-23-1

--- membership/js/endpoints.js
+++
@@ -1,8 +0,0 @@
-// export an array
-// export let months = ['Jan', 'Feb', 'Mar','Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-// // export a constant
-// export const MODULES_BECAME_STANDARD_YEAR = 2015;
-
-
-export const updateContactEndpoint = '/_hcms/api/updateContact';
